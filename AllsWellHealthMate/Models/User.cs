d10c4616ce16e119f4fdb6a9993b765c64bf993a
Ugo <ugongene45@gmail.com>
2024-10-03 23:08:19 -0400

    Configuration changes

---
+++ AllsWellHealthMate/Models/User.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AllsWellHealthMate.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
